fdbe156796396d2b3f505a75353ff8fa20ec2509
siminafildan <simina.fildan04@e-uvt.ro>
2026-02-04 16:32:12 +0200

    load book

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Simina Fildan",
+    author_email="simina.fildan04@e-uvt.ro",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+touch research/trials.ipynb
+
+echo "created successfully"
